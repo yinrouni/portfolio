417bc2cbe736b0e724f2cbc68fe29264361feebc
rouni.yin <yin.ro@husky.neu.edu>
2020-09-25 21:41:49 -0400

    feat: edu

--- src/components/Education.vue
+++ src/components/Education.vue
@@ -9,21 +9,52 @@
     <div class="content">
     <el-timeline>
       <el-timeline-item type="primary" size="large" icon="el-icon-check">
-        <el-card>
-          <h4>更新 Github 模板</h4>
-          <p>王小虎 提交于 2018/4/12 20:46</p>
+        <el-card class="left">
+          <el-row  :gutter="20">
+            <el-col :span="12">
+              <h3>Northeastern University<span>, Boston, USA</span></h3>
+              <p>Khoury College of Computer Sciences</p>
+              <p style="font-weight: bold">Candidate for a Master of Science in Computer Science</p>
+              <p> Sep. 2018 - Dec. 2020 </p>
+            </el-col>
+            <el-col :span="12">
+              <p> GPA 3.8/4.0</p>
+              <p><span style="font-weight: bold">Related Courses:</span> Object-Oriented Design, Database Management System, Discrete Math and Data Structures,
+                Algorithms, Web Development, Android App Development, Machine Learning</p>
+            </el-col>
+          </el-row>
         </el-card>
       </el-timeline-item>
       <el-timeline-item type="primary" size="large">
-        <el-card>
-          <h4>更新 Github 模板</h4>
-          <p>王小虎 提交于 2018/4/3 20:46</p>
+        <el-card class="left">
+          <el-row  :gutter="20">
+            <el-col :span="12">
+              <h3>University of Electronics Science and Technology of China<span>, Chengdu, China</span></h3>
+              <p>School of Microelectronics and Solid State Electronics</p>
+              <p style="font-weight: bold">Bachelor of Science in Applied Chemistry</p>
+              <p>Sep.2014 – Jul.2018</p>
+            </el-col>
+            <el-col :span="12">
+              <p> GPA 3.9/4.0</p>
+              <p><span style="font-weight: bold">Related Courses:</span> Calculus, Algebra,
+                Probability Statistics, Programming in C, MATLAB,  Discrete Math</p>
+            </el-col>
+          </el-row>
         </el-card>
       </el-timeline-item>
       <el-timeline-item type="primary" size="large">
-        <el-card>
-          <h4>更新 Github 模板</h4>
-          <p>王小虎 提交于 2018/4/2 20:46</p>
+        <el-card class="left">
+          <el-row :gutter="20">
+            <el-col :span="12">
+              <h3>King Abdullah University of Science and Technology<span>, Thuwal, Kingdom of Saudi Arabia</span></h3>
+              <p>Sep.2017 - Jan.2018</p>
+            </el-col>
+            <el-col :span="12">
+              <p> Research Assistant</p>
+              <p> Research on Metal Catalyst for CO2 Reduction</p>
+            </el-col>
+          </el-row>
+
         </el-card>
       </el-timeline-item>
     </el-timeline>
@@ -43,6 +74,14 @@ export default {
   .content{
     width: 80%;
     margin: auto;
+  }
+  .content >>> .el-card__body{
+    padding:6px 20px;
+  }
+
+  h3 span{
+    font-weight: normal;
+    font-size: small;
   }
 
 </style>
